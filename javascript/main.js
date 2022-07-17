1a1fb03eff8bbf1a15b448356aa8b1e83fa0c365
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-07-17 11:28:13 -0400

    firt commit.

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+// Show Menu
+
+const navToggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+const close = document.getElementById('x')
+
+navToggle.addEventListener('click', () => nav.classList.add('show'))
+x.addEventListener('click', () => nav.classList.remove('show'))
+
+
